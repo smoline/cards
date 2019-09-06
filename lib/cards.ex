53aa181431d2c09891f60887b291f1e3199018d7
Sherilyn Moline <smoline@gmail.com>
2019-09-06 14:42:47 -0400

    docs generated

--- lib/cards.ex
+++ lib/cards.ex
@@ -4,11 +4,7 @@ defmodule Cards do
     "hi there!"
   end
   @moduledoc """
-  Cards keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+    Provides methods for creating and handling a deck of cards
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
